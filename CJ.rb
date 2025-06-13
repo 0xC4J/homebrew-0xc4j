d2d50eb7e87f0fa67ad5c9f9da5925aed2170d47
0xC4J <cha.hack3r@gmail.com>
2025-06-13 11:32:21 +0700

    Update CJ.rb

--- CJ.rb
+++ CJ.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class C4j < Formula
-  desc "Installer for 0xC4J's development environment using a Brewfile"
+class CJ < Formula
+  desc "Installer for CJ's development environment"
   homepage "https://github.com/0xC4J/homebrew-0xc4j"
-  
-  url "https://github.com/0xC4J/homebrew-0xc4j/archive/refs/tags/v2.0.0.tar.gz"
+
+  url "https://github.com/0xC4J/homebrew-0xc4j/archive/refs/tags/v4.0.0.tar.gz"
   sha256 "5db7298fbce6431e064ebe59443443530f9e6a961f64ae44eb68421f5be784eb"
   
   license "MIT"
