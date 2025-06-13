abb42455544a4430fdfde637f0bad915114256e4
0xC4J <cha.hack3r@gmail.com>
2025-06-13 11:56:34 +0700

    Update cj.rb

--- cj.rb
+++ cj.rb
@@ -3,30 +3,29 @@
 class Cj < Formula
   desc "Installer for cj's development environment using a Brewfile"
   homepage "https://github.com/0xC4J/homebrew-0xc4j"
-
   url "https://github.com/0xC4J/homebrew-0xc4j/archive/refs/tags/v7.0.0.tar.gz"
   sha256 "a4253a0a22df5f619c5e8630d6840a9c3da90b295a19669d79607aca97c6ef31"
-
   license "MIT"
 
   def install
     brewfile_url = "https://raw.githubusercontent.com/0xC4J/homebrew-0xc4j/main/Brewfile"
     
     ohai "Downloading Brewfile from your GitHub..."
-    system "curl", "-fsSL", "-o", "Brewfile.downloaded", brewfile_url
-    
-    unless File.exist?("Brewfile.downloaded")
+    system "curl", "-fsSL", "-o", buildpath/"Brewfile.downloaded", brewfile_url
+
+    unless File.exist?(buildpath/"Brewfile.downloaded")
       odie "Failed to download Brewfile from #{brewfile_url}"
     end
 
     ohai "Installing from downloaded Brewfile..."
-    system "brew", "bundle", "install", "--file=Brewfile.downloaded"
+    system "brew", "bundle", "install", "--file=#{buildpath}/Brewfile.downloaded"
 
-    touch "cj.installed"
+    # Record installation
+    (buildpath/"cj.installed").write(Time.now.to_s)
     prefix.install "cj.installed"
   end
 
   test do
-    assert_predicate prefix/"cj.installed", :exist?
+    assert_predicate prefix/"cj.installed", :exist?, "Install marker missing"
   end
 end
