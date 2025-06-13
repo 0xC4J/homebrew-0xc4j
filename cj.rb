8b45f109c8a1739ac9dd6fbac136cbe0afe78510
0xC4J <cha.hack3r@gmail.com>
2025-06-13 11:41:00 +0700

    Update cj.rb

--- cj.rb
+++ cj.rb
@@ -4,12 +4,10 @@ class Cj < Formula
   desc "Installer for cj's development environment"
   homepage "https://github.com/0xC4J/homebrew-0xc4j"
 
-  url "https://github.com/0xC4J/homebrew-0xc4j/archive/refs/tags/v4.0.0.tar.gz"
-  sha256 "2c7d2b90da87794a2e59d7fe4fce4efe1c12d07063d84d51bff87c875dc491fe"
-  
-  license "MIT"
+  url "https://github.com/0xC4J/homebrew-0xc4j/archive/refs/tags/v5.0.0.tar.gz"
+  sha256 "YOUR_NEXT_SHA256_HASH_HERE"
 
-  depends_on "homebrew/bundle"
+  license "MIT"
 
   def install
     brewfile_url = "https://raw.githubusercontent.com/0xC4J/homebrew-0xc4j/main/Brewfile"
@@ -18,10 +16,11 @@ class Cj < Formula
 
     system "brew", "bundle", "install", "--file=#{brewfile_url}"
 
-    prefix.install "Brewfile.installed"
+    touch "cj.installed"
+    prefix.install "cj.installed"
   end
 
   test do
-    assert_predicate prefix/"Brewfile.installed", :exist?
+    assert_predicate prefix/"cj.installed", :exist?
   end
 end
